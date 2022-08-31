e54692a733ee6b64705f66f5fc185f0b1da70113
ZVA-23 <zva-23@ukr.net>
2022-08-31 20:41:31 +0300

    add files 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
+// Виконуй це завдання у файлах 01 - gallery.html і 01 - gallery.js.Розбий його на
+// декілька підзавдань:
+
+// Додай бібліотеку SimpleLightbox як залежність проекту, використовуючи npm(посилання
+//     а CDN з твоєї минулої роботи більше не потрібне).
+// Використовуй свій JavaScript код з попередньої домашньої роботи, але виконай
+// рефакторинг з урахуванням того, що бібліотека була встановлена через npm(синтаксис
+//     import /export).
+// Для того щоб підключити CSS код бібліотеки в проект, необхідно додати ще один імпорт,
+//     крім того, що описаний в документації.
+
+// // Описаний в документації
+// import SimpleLightbox from "simplelightbox";
+// // Додатковий імпорт стилів
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const gallaryList = document.querySelector('.gallery');
+
+const makeGalleryMarcup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>`
+  )
+  .join('');
+gallaryList.insertAdjacentHTML('beforeend', makeGalleryMarcup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  // captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
